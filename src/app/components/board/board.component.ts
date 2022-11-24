b081a04479316e67973ada9ccdc63e509b910e20
nrusnac <n.rusnac@student.utwente.nl>
2022-11-24 09:35:29 +0100

    Created map for representing colors in board template

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+
+type Color = "black" | "white"
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
+
+  colors = new Map<Color, string>([
+    ["white", "#b58863"],
+    ["black", "#f0d9b5"]
+  ])
 
   constructor() { }
 

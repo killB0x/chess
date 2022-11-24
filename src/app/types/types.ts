cfb9d8bab01c8541b9bd6c4032f3da4ec13763b0
nrusnac <n.rusnac@student.utwente.nl>
2022-11-24 13:05:37 +0100

    Created type for chess pieces

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -1,3 +1,4 @@
 export type Color = "black" | "white"
 export type xCoordinate = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h"
 export type yCoordinate = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8"
+export type cellType = "pawn" | "rook" | "queen" | "knight" | "bishop" | "king" | "empty"
--- src/model/cell.model.ts
+++ src/model/cell.model.ts
@@ -1,11 +1,14 @@
-import { Color, xCoordinate, yCoordinate } from "src/app/types/types";
+import { cellType, Color, xCoordinate, yCoordinate } from "src/app/types/types";
 
 export class Cell {
+  type: cellType
   color: Color
   x: xCoordinate
   y: yCoordinate
 
-  constructor (color: Color, x:xCoordinate, y: yCoordinate) {
+
+  constructor (color: Color, x:xCoordinate, y: yCoordinate, type?:cellType) {
+    this.type = type ?? "empty"
     this.color = color
     this.x = x
     this.y = y
